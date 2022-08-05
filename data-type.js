d810695943e159e97b23667aa8be20511a7e0398
touhidcodes <touhidcodes@gmail.com>
2022-08-05 21:00:06 +0600

    Data Types

--- data-type.js
+++ data-type.js
@@ -6,3 +6,9 @@ console.log(numberDataType);
 
 var stringDataType = "This is a String";
 console.log(stringDataType);
+
+var numberDataType = 24;
+console.log(typeof numberDataType);
+
+var stringDataType = "This is a String";
+console.log(typeof stringDataType);
