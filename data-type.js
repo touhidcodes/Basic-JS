dd034cb1821c6878c9117e1e25d07e85522912d8
touhidcodes <touhidcodes@gmail.com>
2022-08-05 22:11:24 +0600

    Data Types

--- data-type.js
+++ data-type.js
@@ -12,3 +12,14 @@ console.log(typeof numberDataType);
 
 var stringDataType = "This is a String";
 console.log(typeof stringDataType);
+
+const cars = ["Saab", "Volvo", "BMW"];
+console.log(cars[0]);
+
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 50,
+  eyeColor: "blue",
+};
+console.log(person.firstName + " is " + person.age + " years old.");
