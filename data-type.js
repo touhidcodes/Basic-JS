7752a380bc630474102aa85fc6d1debaceaaa9b0
touhidcodes <touhidcodes@gmail.com>
2022-08-05 20:08:58 +0600

    Data Type

---
+++ data-type.js
@@ -0,0 +1,8 @@
+//var undefinedDataType = x;
+//console.log(undefinedDataType);
+
+var numberDataType = 24;
+console.log(numberDataType);
+
+var stringDataType = "This is a String";
+console.log(stringDataType);
