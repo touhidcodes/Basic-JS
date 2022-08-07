e3ce7502d2347c84f860b9d9b94d56897bbbd4eb
touhidcodes <touhidcodes@gmail.com>
2022-08-07 17:52:06 +0600

    Data Types

--- data-type.js
+++ data-type.js
@@ -1,21 +1,28 @@
+// Undefined Data Type
 //var undefinedDataType = x;
 //console.log(undefinedDataType);
 
+// Number Data Type
 var numberDataType = 24;
 console.log(numberDataType);
 
+// String Data Type
 var stringDataType = "This is a String";
 console.log(stringDataType);
 
+// Declare and Output Number Data Type
 var numberDataType = 24;
 console.log(typeof numberDataType);
 
+// Declare and Output String Data Type
 var stringDataType = "This is a String";
 console.log(typeof stringDataType);
 
+// Array Data Type
 const cars = ["Saab", "Volvo", "BMW"];
 console.log(cars[0]);
 
+// Object Data Type
 const person = {
   firstName: "John",
   lastName: "Doe",
--- number-method.js
+++ number-method.js
@@ -1,8 +1,18 @@
-var number = 24;
+var number1 = 24;
 var number2 = 25.5;
-console.log(number + number2);
-number2 = parseInt(number2);
-console.log(number + number2);
+console.log(number1 + number2);
+
+// Parse Integer Type
+var numberInt1 = 24;
+var numberString1 = "25.5";
+numberString1 = parseInt(numberString1);
+console.log(numberInt1 + numberString1);
+
+// Parse Float Type
+var numberInt2 = 25;
+var numberString2 = "25.5";
+numberString2 = parseFloat(numberString2);
+console.log(numberInt2 + numberString2);
 
 var num1 = 0.3;
 var num2 = 0.5;
