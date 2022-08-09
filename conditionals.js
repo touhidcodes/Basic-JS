9b4926a546a0e1558720be344e5e266aa8ac7f0e
touhidcodes <touhidcodes@gmail.com>
2022-08-09 23:38:54 +0600

    g

---
+++ conditionals.js
@@ -0,0 +1,23 @@
+var result = 74;
+
+if ((result > 80, (result = 80))) {
+  console.log("A+");
+} else if ((result > 75, result < 70((result = 75)))) {
+  console.log("A");
+} else if ((result > 70, (result = 70))) {
+  console.log("A-");
+} else if ((result > 65, (result = 65))) {
+  console.log("B+");
+} else if ((result > 60, (result = 60))) {
+  console.log("B");
+} else if ((result > 55, (result = 55))) {
+  console.log("B-");
+} else if ((result > 50, (result = 50))) {
+  console.log("C+");
+} else if ((result > 45, (result = 45))) {
+  console.log("C");
+} else if ((result > 40, (result = 40))) {
+  console.log("D");
+} else {
+  console.log("F");
+}
--- math-method.js
+++ math-method.js
@@ -1,5 +1,5 @@
 var number1 = 20;
 var number2 = 25;
 
-number2 = number2.Math.abs();
+number2 = Math.abs();
 console.log(number2);
