22122e6c93919dda1f0a6df7c55300455bd73e9a
touhidcodes <touhidcodes@gmail.com>
2022-08-12 21:52:50 +0600

    Conditionals

---
+++ conditionals.js
@@ -0,0 +1,8 @@
+var price = 10;
+if (price <= 10) {
+  console.log("I will eat Banana.");
+} else if (price <= 20 && price >= 10) {
+  console.log("I will eat Butter Bun insist.");
+} else {
+  console.log("Price is to high. I dont eat anything.");
+}
