d52f253320cf0050df72a5b53095fea6c3f56907
touhidcodes <touhidcodes@gmail.com>
2022-08-19 00:41:58 +0600

    conditionals.js

---
+++ operators.js
@@ -0,0 +1,24 @@
+var x = 10;
+var y = 4;
+var result1 = x + y;
+console.log(result1);
+
+var result2 = x - y;
+console.log(result2);
+
+var result3 = x * y;
+console.log(result3);
+
+var result4 = x / y;
+console.log(result4);
+
+var result5 = x % y;
+console.log(result5);
+
+var incriment = 20;
+incriment++;
+console.log(incriment);
+
+var decriment = 20;
+decriment--;
+console.log(decriment);
