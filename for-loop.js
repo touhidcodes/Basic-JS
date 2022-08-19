659435d83864267bd02a25556c04669435bd4766
touhidcodes <touhidcodes@gmail.com>
2022-08-20 01:11:52 +0600

    for loop

--- for-loop.js
+++ for-loop.js
@@ -1,11 +1,3 @@
-//for (i = 0; i < 10; i++) {
-//console.log(i);
-//}
-
-var nums = [10, 20, 30, 40, 50, 60, 70, 80, 90];
-
-for (var i = 0; i < nums.length; i++);
-{
-  var element = nums[i];
-  console.log(element);
+for (i = 0; i < 10; i++) {
+  console.log("love you");
 }
