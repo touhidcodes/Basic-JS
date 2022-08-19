d8e30aa46274c3ae6539e7c1490e34c9b76d3af8
touhidcodes <touhidcodes@gmail.com>
2022-08-20 01:33:04 +0600

    switch loop

---
+++ switch-loop.js
@@ -0,0 +1,21 @@
+var orange = "This is orange";
+
+switch (orange) {
+  case papaya:
+    console.log("This is Papaya");
+    break;
+  case orange:
+    console.log("This is Orange");
+    break;
+  case banana:
+    console.log("This is Banana");
+    break;
+  case guava:
+    console.log("This is guava");
+    break;
+  case lemon:
+    console.log("This is lemon");
+    break;
+  default:
+    console.log("None of these");
+}
