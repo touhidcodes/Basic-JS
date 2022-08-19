b23e6f931f6446df152bcef6ee441eb4a5a55feb
touhidcodes <touhidcodes@gmail.com>
2022-08-20 01:17:34 +0600

    forloop

--- for-loop.js
+++ for-loop.js
@@ -1,3 +1,17 @@
 for (i = 0; i < 10; i++) {
   console.log("love you");
 }
+
+var friends = ["karim", "Rahim", "Lutfor", "Hira", "Kolim"];
+
+for (var i = 0; i < friends.length; i++) {
+  var output = friends[i];
+  console.log(output);
+}
+
+var number = [56, 76, 77, 65, 34, 24, 56, 78, 99, 07, 45, 09, 44, 23, 56];
+
+for (var i = 0; i < number.length; i++) {
+  var element = number[i];
+  console.log(element);
+}
