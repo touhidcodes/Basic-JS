49cc2dbaae34faedd769413eb7a42dbffee8180f
touhidcodes <touhidcodes@gmail.com>
2022-08-20 00:09:44 +0600

    array.js

---
+++ array.js
@@ -0,0 +1,21 @@
+//var decleare
+var nameOfFriends = ["Sakib", "Jisan", "Mithun", "Rabbi", "Mamun"];
+console.log(nameOfFriends);
+
+//add element in last
+var add = nameOfFriends.push("Sassoto");
+add = nameOfFriends.push("Rakib");
+console.log(nameOfFriends);
+
+//removes element from last
+var remove = nameOfFriends.pop();
+console.log(nameOfFriends);
+
+//add element in first
+add = nameOfFriends.unshift("Kiran");
+add = nameOfFriends.unshift("Mohim");
+console.log(nameOfFriends);
+
+//removes element in first
+remove = nameOfFriends.shift();
+console.log(nameOfFriends);
