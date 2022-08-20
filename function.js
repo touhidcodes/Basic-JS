5edc49b21bd02e1ce1c6ab769d653e7d8636f30c
touhidcodes <touhidcodes@gmail.com>
2022-08-20 11:38:57 +0600

    function

--- function.js
+++ function.js
@@ -5,3 +5,11 @@ function tripleIt(num1) {
 
 var output2 = tripleIt(2);
 console.log(output2);
+
+function myFunction(no1, no2, no3) {
+  var total = no1 + no2 * no3;
+  return total;
+}
+
+var newOutput = myFunction(3, 3, 5);
+console.log(newOutput);
