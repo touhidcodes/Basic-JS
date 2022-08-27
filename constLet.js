080abc90f5dd9d2fe4c345a9d8e9abf3bc10c50c
touhidcodes <touhidcodes@gmail.com>
2022-08-27 19:58:40 +0600

    constLet.js

---
+++ constLet.js
@@ -0,0 +1,13 @@
+var person = "karim Benzemar";
+if (person.length > 4) {
+  person = "Dub";
+}
+
+console.log(person);
+
+const country = "Bangladesh";
+console.log(country);
+
+let age = 34;
+age = 2;
+console.log(age);
